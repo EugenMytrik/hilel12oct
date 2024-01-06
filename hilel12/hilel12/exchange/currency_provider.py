167054652646e6da39197ecce9cb7bf735b59af3
Eugene Mytrik <104303531+EugenMytrik@users.noreply.github.com>
2024-01-06 18:41:02 +0200

    Add files via upload

---
+++ hilel12/hilel12/exchange/currency_provider.py
@@ -0,0 +1,152 @@
+import dataclasses
+from abc import ABC, abstractmethod
+
+import requests
+
+
+@dataclasses.dataclass
+class SellBuy:
+    sell: float
+    buy: float
+
+
+class RateNotFound(Exception):
+    pass
+
+
+class ProviderBase(ABC):
+    name = None
+
+    def __init__(self, currency_from: str, currency_to: str):
+        self.currency_from = currency_from
+        self.currency_to = currency_to
+
+    @abstractmethod
+    def get_rate(self) -> SellBuy:
+        pass
+
+
+class MonoProvider(ProviderBase):
+    name = "monobank"
+
+    iso_from_country_code = {
+        "UAH": 980,
+        "USD": 840,
+        "EUR": 978,
+    }
+
+    def get_rate(self) -> SellBuy:
+        url = "https://api.monobank.ua/bank/currency"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        currency_from_code = self.iso_from_country_code[self.currency_from]
+        currency_to_code = self.iso_from_country_code[self.currency_to]
+
+        for currency in response.json():
+            if (
+                currency["currencyCodeA"] == currency_from_code
+                and currency["currencyCodeB"] == currency_to_code
+            ):
+                value = SellBuy(
+                    sell=float(currency["rateSell"]), buy=float(currency["rateBuy"])
+                )
+                return value
+        raise RateNotFound(
+            f"Cannot find rate from {self.currency_from} to {self.currency_to} in provider {self.name}"
+        )
+
+
+class PrivatbankProvider(ProviderBase):
+    name = "privatbank"
+
+    def get_rate(self) -> SellBuy:
+        url = "https://currate.ru/api/?get=rates&pairs=USDRUB&key=d377eb02e5c2f5d5fbef7c61ac8ed863"
+        response = requests.get(url)
+        response.raise_for_status()
+        for currency in response.json():
+            if (
+                currency["ccy"] == self.currency_from
+                and currency["base_ccy"] == self.currency_to
+            ):
+                value = SellBuy(
+                    buy=float(currency["buy"]), sell=float(currency["sale"])
+                )
+                return value
+        raise RateNotFound(
+            f"Cannot find rate from {self.currency_from} to {self.currency_to} in provider {self.name}"
+        )
+
+
+class VkurseProvider(ProviderBase):
+    name = "vkurse"
+
+    iso_from_country_code = {
+        "USD": "Dollar",
+        "EUR": "Euro",
+    }
+
+    def get_rate(self) -> SellBuy:
+        url = "https://vkurse.dp.ua/course.json"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        currency_from_code = self.iso_from_country_code[self.currency_from]
+
+        for currency, data in dict(response.json()).items():
+            if currency == currency_from_code and self.currency_to == "UAH":
+                value = SellBuy(sell=float(data["sell"]), buy=float(data["buy"]))
+                return value
+        raise RateNotFound(
+            f"Exchange rate not found for {self.currency_from} to {self.currency_to}"
+        )
+
+
+class NBUStatProvider(ProviderBase):
+    name = "nbustat"
+
+    def get_rate(self) -> SellBuy:
+        url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        for currency in response.json():
+            if currency["cc"] == self.currency_from and self.currency_to == "UAH":
+                value = SellBuy(
+                    sell=float(currency["rate"]), buy=float(currency["rate"])
+                )
+                return value
+        raise RateNotFound(
+            f"Exchange rate not found for {self.currency_from} to {self.currency_to}"
+        )
+
+
+class MinfinProvider(ProviderBase):
+    name = "minfin"
+    rename_currency = {
+        "USD": "usd",
+        "EUR": "eur",
+    }
+
+    def get_rate(self) -> SellBuy:
+        url = "https://api.minfin.com.ua/mb/2d57c01794142f530091b1330cfbbbf19d4c2dee/"
+
+        response = requests.get(url)
+        response.raise_for_status()
+
+        for currency in response.json():
+            if currency["currency"] == self.rename_currency[self.currency_from]:
+                value = SellBuy(buy=float(currency["ask"]), sell=float(currency["bid"]))
+                return value
+        raise RateNotFound(
+            f"Cannot find rate from {self.currency_from} to {self.currency_to} in provider {self.name}"
+        )
+
+
+PROVIDERS = [
+    MonoProvider,
+    PrivatbankProvider,
+    MinfinProvider,
+    NBUStatProvider,
+    VkurseProvider,
+]
---
+++ hilel12/hilel12/exchange/views.py
@@ -0,0 +1,57 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from .models import Rate
+
+
+def main_view(request):
+    response_data = {
+        "current_rates": [
+            {
+                "id": rate.id,
+                "date": rate.date,
+                "vendor": rate.provider,
+                "currency_a": rate.currency_from,
+                "currency_b": rate.currency_to,
+                "sell": rate.sell,
+                "buy": rate.buy,
+            }
+            for rate in Rate.objects.all()
+        ]
+    }
+    return JsonResponse(response_data)
+
+
+def currency_exchange(request):
+    best_rates = {}
+    currencies = Rate.objects.values_list("currency_from", flat=True).distinct()
+    for currency in currencies:
+        best_rate = (
+            Rate.objects.filter(currency_from=currency).order_by("-sell").first()
+        )
+        best_rates[currency] = best_rate
+
+    context = {"best_rates": best_rates}
+    return render(request, "currency_exchange.html", context)
+
+
+def exchange_result(request):
+    if request.method == "POST":
+        from_currency = request.POST["from_currency"]
+        amount = float(request.POST["amount"])
+
+        best_rate = (
+            Rate.objects.filter(currency_from=from_currency).order_by("-sell").first()
+        )
+
+        if best_rate:
+            result = amount * best_rate.sell
+            return render(
+                request,
+                "exchange_result.html",
+                {"result": result, "currency": from_currency},
+            )
+
+    return render(
+        request, "error.html", {"message": "Помилка в підрахунку обміну валюти"}
+    )
